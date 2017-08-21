b78306fb099cff7a3875e1d844607a61122d2b75
Thiago Miller <tmiller@mochsl.org.br>
2017-08-21 00:39:05 -0300

    Added Quality::Handle to manage database path

--- script/insertdb.pl
+++ script/insertdb.pl
@@ -19,11 +19,15 @@
 #===============================================================================
 
 use My::Base;
-use My::Schema;
 use Path::Class 'file';
 use Getopt::Long;
+use IO::Compress::Gzip 'gzip';
+use IO::Uncompress::Gunzip 'gunzip';
 use Storable qw/nfreeze thaw/;
 use Data::Dumper;
+
+use Quality::Handle;
+my $db = Quality::Handle->new;
 
 my $progname = file(__FILE__)->basename;
 my $db_fn = file(__FILE__)->dir->parent->file('share', 'quality_profile.db');
@@ -33,7 +37,7 @@ my $help = 0;
 $LOG_VERBOSE = 0;
 
 unless (@ARGV) {
-	report();
+	print_report($db->make_report);
 	exit 0;
 }
 	
@@ -57,7 +61,7 @@ unless ($size)              { die "size not defined\n"                => usage()
 unless ($size > 0)          { die "size must be a positive integer\n" => usage() }
 
 log_msg ":: Connecting to database $db_fn";
-my $schema = My::Schema->connect("dbi:SQLite:$db_fn", "", "", { RaiseError => 1, PrintError => 0 });
+my $schema = $db->schema;
 
 log_msg ":: Checking if there is already a sequencing-system '$sequencing_system' ...";
 my $seq_sys_rs = $schema->resultset('SequencingSystem')->find({ name => $sequencing_system });
@@ -82,7 +86,9 @@ if ($seq_sys_rs) {
 log_msg ":: Indexing '$matrix_file' ...";
 my $arr = index_quality($matrix_file, $size);
 log_msg ":: Converting array to bytes ...";
-my $bytes = freeze $arr;
+my $bytes = nfreeze $arr;
+log_msg ":: Compressing bytes ...";
+gzip \$bytes => \my $compressed;
 
 unless ($seq_sys_rs) {
 	log_msg ":: Creating sequencing-system entry for '$sequencing_system' ...";
@@ -93,10 +99,20 @@ log_msg ":: Storing quality matrix entry ...";
 my $quality_rs = $seq_sys_rs->create_related( qualities => {
 	source => $source,
 	size   => $size,
-	matrix => $bytes
+	matrix => $compressed
 });
 
 log_msg ":: FINITO!";
+
+#my $quality_rs2 = $schema->resultset('Quality')->find(
+#	{ 'sequencing_system.name' => $sequencing_system, size => $size },
+#	{ prefetch => ['sequencing_system'] }
+#);
+#
+#my $c = $quality_rs2->matrix;
+#gunzip \$c => \my $u;
+#my $str = thaw $u;
+#print Dumper($str);
 
 sub usage {
 	say STDERR "usage $progname <MATRIX> -q [sequencing system] -s [size] -f [source]";
@@ -121,25 +137,18 @@ sub index_quality {
 	return \@arr;
 }
 
-sub report {
-	my $schema = My::Schema->connect("dbi:SQLite:$db_fn", "", "", { RaiseError => 1, PrintError => 0 });
-	my $quality_rs = $schema->resultset('Quality')->search(
-		{},
-		{
-			order_by => [{ -asc => 'sequencing_system.name' }, { -asc => 'size'}],
-			prefetch => ['sequencing_system']
-		}
-	);
+sub print_report {
+	my $report_ref = shift;
+	return if not defined $report_ref;
 
 	my $format = "\t%*s\t%*s\t%*s\n";
 	my ($s1, $s2, $s3) = map {length} qw/sequencing_system/x3;
-	printf $format => $s1, "sequencing system", $s2, "size", $s3, "source"
-		if $quality_rs;
+	printf $format => $s1, "sequencing system", $s2, "size", $s3, "source";
 
-	while (my $quality = $quality_rs->next) {
-		my $sequencing_system = $quality->sequencing_system->name;
-		my $size = $quality->size;
-		my $source = $quality->source;
-		printf $format => $s1, $sequencing_system, $s2, $size, $s3, $source;
+	for my $sequencing_system (sort keys %$report_ref) {
+		my $attr = $report_ref->{$sequencing_system};
+		for my $entry (sort { $a->{size} <=> $b->{size} } @$attr) {
+			printf $format => $s1, $sequencing_system, $s2, $entry->{size}, $s3, $entry->{source};
+		}
 	}
 }
