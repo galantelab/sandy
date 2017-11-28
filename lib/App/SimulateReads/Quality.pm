f2a8a207ef37601efa304505fbe3ab3022f9c48e
Thiago Miller <tmiller@mochsl.org.br>
2017-11-28 00:46:11 -0200

    Quality class more cleaner

--- lib/App/SimulateReads/Quality.pm
+++ lib/App/SimulateReads/Quality.pm
@@ -6,22 +6,42 @@ use App::SimulateReads::Quality::Handle;
 
 # VERSION
 
-#-------------------------------------------------------------------------------
-#  Moose attributes
-#-------------------------------------------------------------------------------
-has 'quality_profile'    => (is => 'ro', isa => 'My:QualityP', required => 1, coerce => 1);
-has 'read_size'          => (is => 'ro', isa => 'My:IntGt0',   required => 1);
+has 'quality_profile' => (
+	is         => 'ro',
+	isa        => 'My:QualityP',
+	required   => 1,
+	coerce     => 1
+);
+
+has 'read_size' => (
+	is         => 'ro',
+	isa        => 'My:IntGt0',
+	required   => 1
+);
+
 has '_quality_by_system' => (
 	is         => 'ro',
 	isa        => 'My:QualityH',
 	builder    => '_build_quality_by_system',
 	lazy_build => 1
 );
-has '_gen_quality'      => (
+
+has '_gen_quality' => (
 	is         => 'ro',
 	isa        => 'CodeRef',
 	builder    => '_build_gen_quality',
 	lazy_build => 1
+);
+
+has '_phred_score' => (
+	traits     => ['Array'],
+	isa        => 'ro',
+	isa        => 'ArrayRef',
+	builder    => '_build_phred_score',
+	handles    => {
+		_count_phred_score => 'count',
+		_get_phred_score   => 'get'
+	}
 );
 
 sub BUILD {
@@ -30,42 +50,6 @@ sub BUILD {
 	$self->_quality_by_system if $self->quality_profile ne 'poisson';
 }
 
-#-------------------------------------------------------------------------------
-#  Phred score table for poisson distribution simulation
-#-------------------------------------------------------------------------------
-my @PHRED_SCORE = (
-	{
-		score  => ['I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', '@', '?'],
-		size   => 11,
-		ratio  => 1.5
-	},
-	{
-		score  => ['>', '=', '<', ';', ':', '9', '8', '7', '6', '5'],
-		size   => 10,
-		ratio  => 2
-	},
-	{
-		score  => ['4', '3', '2', '1', '0', '/', '.', '-', ',', '+'],
-		size   => 10,
-		ratio  => 2
-	},
-	{
-		score  => ['*', ')', '(', '\'', '&', '%', '$', '#', '"', '!'],
-		size   => 10,
-		ratio  => 1
-	}
-);
-
-#===  CLASS METHOD  ============================================================
-#        CLASS: Quality
-#       METHOD: _build_gen_quality (BUILDER)
-#   PARAMETERS: Void
-#      RETURNS: $fun CodeRef
-#  DESCRIPTION: Dynamic linkage for quality profile generator 
-#       THROWS: no exceptions
-#     COMMENTS: none
-#     SEE ALSO: n/a
-#===============================================================================
 sub _build_gen_quality {
 	my $self = shift;
 	my $fun;
@@ -80,47 +64,50 @@ sub _build_gen_quality {
 	}
 
 	return $fun;
-} ## --- end sub _build_gen_quality
+}
 
-#===  CLASS METHOD  ============================================================
-#        CLASS: Quality
-#       METHOD: _build_quality_by_system (BUILDER)
-#   PARAMETERS: Void
-#      RETURNS: $quality_by_system My:QualityH
-#  DESCRIPTION: Searches into the paths for quality_profile.perldata where is
-#               found the quality distribution for a given system
-#       THROWS: If quality_profile not found, or a given system is not stored
-#               in the database, throws an exception
-#     COMMENTS: none
-#     SEE ALSO: n/a
-#===============================================================================
+sub _build_phred_score {
+	my $self = shift;
+
+	my @phred_score = (
+		{
+			score  => ['I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', '@', '?'],
+			size   => 11,
+			ratio  => 1.5
+		},
+		{
+			score  => ['>', '=', '<', ';', ':', '9', '8', '7', '6', '5'],
+			size   => 10,
+			ratio  => 2
+		},
+		{
+			score  => ['4', '3', '2', '1', '0', '/', '.', '-', ',', '+'],
+			size   => 10,
+			ratio  => 2
+		},
+		{
+			score  => ['*', ')', '(', '\'', '&', '%', '$', '#', '"', '!'],
+			size   => 10,
+			ratio  => 1
+		}
+	);
+
+	return \@phred_score;
+}
+
 sub _build_quality_by_system {
 	my $self = shift;
 	my $db = App::SimulateReads::Quality::Handle->new;
 	my ($matrix, $deepth) = $db->retrievedb($self->quality_profile, $self->read_size);
 	return { matrix => $matrix, deepth => $deepth };
-} ## --- end sub _build_quality_by_system
+}
 
 sub gen_quality {
 	my $self = shift;
 	my $gen_quality = $self->_gen_quality;
 	return $gen_quality->();
-} ## --- end sub gen_quality
+}
 
-#===  CLASS METHOD  ============================================================
-#        CLASS: Quality
-#       METHOD: _gen_quality_by_system (PRIVATE)
-#   PARAMETERS: Void
-#      RETURNS: \$quality Ref Str
-#  DESCRIPTION: Calcultes a quality string by raffle inside a quality matrix -
-#               where each position is a vector encoding a distribution. So
-#               if the string length is 100 bases, it needs to raffle 100 times.
-#               The more present is a given quality, the more chance to be raffled
-#               it will be
-#       THROWS: no exceptions
-#     COMMENTS: none
-#     SEE ALSO: n/a
-#===============================================================================
 sub _gen_quality_by_system {
 	my $self = shift;
 
@@ -133,49 +120,24 @@ sub _gen_quality_by_system {
 	}
 
 	return \$quality;
-} ## --- end sub _gen_quality_by_system
+}
 
-#===  CLASS METHOD  ============================================================
-#        CLASS: Quality
-#       METHOD: _gen_quality_by_poisson_dist
-#   PARAMETERS: Void
-#      RETURNS: \$quality Ref Str
-#  DESCRIPTION: Calculates quality based in a cumulative poisson distribution
-#       THROWS: no exceptions
-#     COMMENTS: It uses the @PHRED_SCORE table to simulate a proportion score
-#               similar to the poisson distribution
-#     SEE ALSO: _poisson_dist
-#===============================================================================
 sub _gen_quality_by_poisson_dist {
 	my $self = shift;
 	my $quality;
-	return $self->_poisson_dist(\$quality, $self->read_size, scalar @PHRED_SCORE);
-} ## --- end sub _gen_quality_by_poisson_dist
+	return $self->_poisson_dist(\$quality, $self->read_size, $self->_count_phred_score);
+}
 
-#===  CLASS METHOD  ============================================================
-#        CLASS: Quality
-#       METHOD: _poisson_dist
-#   PARAMETERS: $quality_ref Str Ref, $size Int > 0, $countdown Int >= 0
-#      RETURNS: $quality_ref Str Ref
-#  DESCRIPTION: Recursive routine that generates a quality string based in a
-#               uniform random raffle into @PHRED_SCORE partitions. It works as
-#               a poisson CDF
-#       THROWS: no exceptions
-#     COMMENTS: none
-#     SEE ALSO: _gen_quality_by_poisson_dist
-#===============================================================================
 sub _poisson_dist {
 	my ($self, $quality_ref, $size, $countdown) = @_;
 	return $quality_ref if not $countdown;
 
-	my $ratio = $PHRED_SCORE[4 - $countdown]{ratio};
-	my $part = int($size / $ratio) + ($size % $ratio);
-	my $score = $PHRED_SCORE[4 - $countdown]{score};
-	my $score_size = $PHRED_SCORE[4 - $countdown]{size};
+	my $phred_score = $self->_get_phred_score($self->_count_phred_score - $countdown);
+	my $part = int($size / $phred_score->{ratio}) + ($size % $phred_score->{ratio});
 
 	for (my $i = 0; $i < $part; $i++) {
-		$$quality_ref .= $score->[int(rand($score_size))];
+		$$quality_ref .= $phred_score->{score}[int(rand($phred_score->{size}))];
 	}
 
 	return $self->_poisson_dist($quality_ref, $size - $part, $countdown - 1);
-} ## --- end sub _poisson_dist
+}
